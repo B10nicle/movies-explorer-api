66ce885956fe53dd4fd28a00be6479a8ad48b1ad
Oleg Khilko <khilkoleg@gmail.com>
2023-06-13 18:18:28 +0300

    fix: config.js

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
 const {
-  MONGODB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
   JWT_SECRET,
   NODE_ENV,
 } = process.env;
 
 module.exports = {
-  MONGODB_URL,
+  MONGO_URL,
   JWT_SECRET,
   NODE_ENV,
 };
