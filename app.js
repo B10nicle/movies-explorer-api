1efbcad0834779171e2b472ca83f47a2353ceb1d
Oleg Khilko <khilkoleg@gmail.com>
2023-06-13 17:18:42 +0300

    feature: add controllers

---
+++ app.js
@@ -0,0 +1,47 @@
+const { requestLogger, errorLogger } = require('./src/middlewares/logger');
+const limiter = require('./src/middlewares/rateLimiter');
+const { MONGO_URL } = require('./src/utils/config');
+const router = require('./src/routes/index');
+const { errors } = require('celebrate');
+const mongoose = require('mongoose');
+const { PORT = 3000 } = process.env;
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.use(requestLogger);
+app.use(limiter);
+app.use(router);
+app.use(errorLogger);
+app.use(errors());
+
+app.use((error, request, response, next) => {
+  const {
+    status = 500,
+    message,
+  } = error;
+  response.status(status)
+    .send({
+      message: status === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+  next();
+});
+
+async function start() {
+  try {
+    await mongoose.connect(MONGO_URL);
+    await app.listen(PORT);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+start()
+  .then(() => console.log(`App has been successfully started!\n${MONGO_URL}\nPort: ${PORT}`));
--- src/error/InternalServerError.js
+++
@@ -1,7 +0,0 @@
-const InternalServerError = (err, req, res, next) => {
-  if (!err.statusCode) res.status(500).send({ message: err.message });
-  else res.status(err.statusCode).send({ message: err.message });
-  next();
-};
-
-module.exports = InternalServerError;
